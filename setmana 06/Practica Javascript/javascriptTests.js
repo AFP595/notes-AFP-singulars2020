d2372716c183c572c05f5f236a36059fb1a823a7
Alex Fernández <afp595@hotmail.com>
2021-01-12 12:47:23 +0100

    Initial commit with notes so far

---
+++ setmana 06/Practica Javascript/javascriptTests.js
@@ -0,0 +1,42 @@
+c = 1; 
+
+f = (c*(9/5)) + 32;
+
+message = "La temperatura és de "+f+" ºF";
+
+console.log(message);
+
+// ---------------------------------------
+
+dies = 3;
+// el teu codi aquí
+console.log("Hi ha " + segons + " segons en " + dies + " dies");
+
+euros = 7;
+// el teu codi aquí
+console.log(euros + " euros són " + dolares + " dòlars");
+
+precio = 100;
+// el teu codi aquí
+console.log("El que costa " + precio + " euros sense IVA, costa " + con_iva + " amb IVA (del 21%)");
+
+// ---------------------------------------
+
+const alto = 4;
+const ancho = 8;
+// tu código aquí
+console.log("un rectángulo de alto " + alto + " y ancho " + ancho + " tiene un perímetro de " + per + " y un área de " + area);
+
+const grados_f = 451
+// tu código aquí
+console.log(grados_f2 + " grados Fahrenheit son " + grados_c2.toFixed(1) + " grados Celsius") // toFixed() fija los decimales
+
+// ---------------------------------------
+
+const radi = 3;
+const PI = 3.14159265;
+console.log("L'àrea d'un cercle de radi " + radi + " és " + area);
+
+const chuches = 17;
+const alumnos = 5;
+console.log("Si somos " + alumnos + " alumons y repartimos " + chuches + " chuches, el Omar se come " + "" /* pon aquí una expresión para las cuches que sobran */);
---
+++ setmana 06/Practica Javascript/numberOutputSwitch.js
@@ -0,0 +1,9 @@
+a = 3;
+b = 4;
+
+temp = a;
+a = b;
+b = temp;
+
+console.log(a);
+console.log(b);
